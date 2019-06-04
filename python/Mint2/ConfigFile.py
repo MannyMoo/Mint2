2234ed16a0a72806476d40b0fc9c75d68ed16c2a
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2019-06-04 12:18:58 +0100

    Update python utils.

--- python/Mint2/ConfigFile.py
+++ python/Mint2/ConfigFile.py
@@ -1,6 +1,31 @@
 '''A class to access parameters in a MINT config file.'''
 
-class ConfigFile(object) :
+import os
+
+def parse_line(line) :
+    '''Parse a line from a config file, taking into account quotes.'''
+    line = line.strip()
+    if not line :
+        return []
+    if line.startswith('"') :
+        istart = 1
+        iend = line.find('"', 1)
+        val = line[1:iend]
+        remainder = line[iend+1:]
+    else :
+        splitline = line.split()
+        val = splitline[0]
+        remainder = ' '.join(splitline[1:])
+    if remainder :
+        return [val] + parse_line(remainder)
+    return [val]
+
+def quotes(val, width = 20) :
+    '''Return a string version of the value enclosed in double quotes, left adjusted to
+    the given width.'''
+    return ('"' + str(val) + '"').ljust(width)
+
+class ConfigFile(dict) :
     '''A class to access parameters in a MINT config file.'''
     
     def __init__(self, *fnames, **parameters) :
@@ -8,11 +33,10 @@ class ConfigFile(object) :
         to use.'''
 
         self.fnames = []
-        self.parameters = {}
 
         for fname in fnames :
             self.add_config_file(fname)
-        self.parameters.update(parameters)
+        self.update(parameters)
 
     def add_config_file(self, fname) :
         '''Add the parameters in the given config file.'''
@@ -21,27 +45,31 @@ class ConfigFile(object) :
             self.add(fname)
             return
 
+        fname = os.path.abspath(os.path.expandvars(fname))
         if not os.path.exists(fname) :
             raise OSError('ConfigFile.add_config_file: Could not find file "{0}"!'.format(fname))
 
         self.fnames.append(fname)
         with open(fname) as configFile :
             for line in configFile :
-                line = line.split()
-                if len(line) == 0 or line[0].startswith('*') :
+                line = line.strip()
+                if not line or line.startswith('*') :
                     continue 
-                self.parameters[line[0]] = line[1:]
+                line = parse_line(line)
+                self[line[0]] = line[1:]
                     
     def add(self, other) :
         '''Combine this config with another.'''
-        self.parameters.update(other.parameters)
+        self.update(other)
         self.fnames += other.fnames
 
-    def write_file(self, fname, namewidth = 20, valwidth = 5) :
+    def write_file(self, fname, namewidth = 30, valwidth = 6) :
         '''Write the configuration to a file.'''
 
         with open(fname, 'w') as f :
-            for par, vals in sorted(self.parameters.items()) :
-                line = par.ljust(namewidth)
-                line += ' '.join(repr(v).ljust(valwidth) for v in vals)
+            for par, vals in sorted(self.items()) :
+                line = quotes(par, namewidth) + ' '
+                line += ' '.join(quotes(v, valwidth) for v in vals)
                 f.write(line + '\n')
+
+    
--- python/Mint2/utils.py
+++ python/Mint2/utils.py
@@ -3,16 +3,25 @@
 import os, subprocess
 from Mint2.ConfigFile import ConfigFile
 
-def run_job(exe, workingdir, *configs, **parameters) :
+def run_job(exe, workingdir, configs = [], parameters = {}, stdout = 'stdout', stderr = 'stderr') :
     '''Run a MINT executable with the given config files/parameters.'''
 
+    if configs and isinstance(configs, (str, ConfigFile)) :
+        configs = [configs]
+
     pwd = os.getcwd()
+    os.chdir(workingdir)
+
     conf = ConfigFile(*configs, **parameters)
     conf.write_file('config.txt')
-    stdout = open('stdout', 'w')
-    stderr = open('stderr', 'w')
-    retval = subprocess.call(app + ' < config.txt', shell = True, stdout = stdout, stderr = stderr)
-    stdout.close()
-    stderr.close()
+    fstdout = open(stdout, 'w')
+    if stdout == stderr :
+        fstderr = fstdout
+    else :
+        fstderr = open(stderr, 'w')
+    retval = subprocess.call(exe + ' < config.txt', shell = True, stdout = fstdout, stderr = fstderr)
+    fstdout.close()
+    fstderr.close()
+    os.chdir(pwd)
     return retval
 
