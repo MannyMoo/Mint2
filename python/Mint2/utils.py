e2e5b88f1ec2f69a64ab5a6c2a3565227826263b
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2019-06-04 18:58:35 +0100

    Add gen_time_dependent

--- python/Mint2/utils.py
+++ python/Mint2/utils.py
@@ -25,3 +25,48 @@ def run_job(exe, workingdir, configs = [], parameters = {}, stdout = 'stdout', s
     os.chdir(pwd)
     return retval
 
+def gen_time_dependent(name, integratorsdir, mintdatadir, configs = [], **parameters) :
+    '''Generate time dependent MINT MC with genTimeDependent.exe.'''
+
+    if not configs and not parameters :
+        raise ValueError('Must give at least one config file or pass the config via **parameters!')
+
+    if isinstance(configs, str) :
+        configs = [configs]
+    config = ConfigFile(*configs, **parameters)
+    outputdir = os.path.join(integratorsdir, name)
+    if not os.path.exists(outputdir) :
+        os.makedirs(outputdir)
+    integsdir = os.path.join(outputdir, 'integrators')
+    config['integratorsDirectory'] = [integsdir]
+
+    datadir = os.path.join(mintdatadir, name)
+    if not os.path.exists(datadir) :
+        os.makedirs(datadir)
+    config['outputFileName'] = [os.path.join(datadir, os.path.split(config['outputFileName'][0])[1])]
+
+    return run_job(exe = 'genTimeDependent.exe', workingdir = outputdir, configs = config,
+                   stdout = 'stdout', stderr = 'stdout')
+
+def gen_time_dependent_main(defaultconfigs, defaultintegratorsdir, defaultdatadir) :
+    '''Main function to generate time dependent MINT MC.'''
+    from argparse import ArgumentParser
+    import sys
+
+    parser = ArgumentParser()
+    parser.add_argument('name', help = 'Name of the dataset to generate')
+    parser.add_argument('--configs', nargs = '*', help = 'Config files to use', default = defaultconfigs)
+    parser.add_argument('--integratorsdir', help = 'Integrators directory', default = defaultintegratorsdir)
+    parser.add_argument('--mintdatadir', help = 'Directory to save generated data', default = defaultdatadir)
+    
+    args, remainder = parser.parse_known_args()
+    variableslists = {}
+    for arg in remainder :
+        if arg.startswith('--') :
+            varargs = []
+            variableslists[arg[2:]] = varargs
+            continue
+        varargs.append(arg)
+
+    sys.exit(gen_time_dependent(name = args.name, configs = args.configs, integratorsdir = args.integratorsdir,
+                                mintdatadir = args.mintdatadir, **variableslists))
