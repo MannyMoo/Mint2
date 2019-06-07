1d9ea5ba0d086ab0271c80e01c497bec23d33f96
Michael Alexander <Michael.Alexander@glasgow.ac.uk>
2019-06-07 18:20:01 +0100

    Add function to calculate cross terms.

--- Mint/PhaseDifferenceCalc.h
+++ Mint/PhaseDifferenceCalc.h
@@ -5,13 +5,18 @@
 #include <memory>
 #include "Mint/DalitzEvent.h"
 #include "Mint/FitAmpSum.h"
+#include <complex>
 
 class PhaseDifferenceCalc {
  public :
   PhaseDifferenceCalc(const DalitzEventPattern&, const char* fname=0) ;
   
   // Can't be made const as IReturnComplexForEvent::ComplexVal is not const.
+  // Returns the phase of A/Abar for the given point in phase space.
   double phase_difference(IDalitzEvent&) ;
+
+  // Returns the cross term A* x Abar fro the given point in phase space.
+  std::complex<double> cross_term(IDalitzEvent&) ;
  private :
   FitAmpSum m_model ;
   FitAmpSum m_cpmodel ;
--- src/Utils/PhaseDifferenceCalc.cpp
+++ src/Utils/PhaseDifferenceCalc.cpp
@@ -6,9 +6,17 @@ PhaseDifferenceCalc::PhaseDifferenceCalc(const DalitzEventPattern& pat, const ch
 {}
 
 // Can't be made const as IReturnComplexForEvent::ComplexVal is not const.
+// Returns the phase of A/Abar for the given point in phase space.
 double PhaseDifferenceCalc::phase_difference(IDalitzEvent& evt) {
   std::complex<double> val = m_model.ComplexVal(evt) ;
   std::complex<double> cpval = m_cpmodel.ComplexVal(evt) ;
   val /= cpval ;
   return std::arg(val) ;
+}
+
+// Returns the cross term A* x Abar fro the given point in phase space.
+std::complex<double> PhaseDifferenceCalc::cross_term(IDalitzEvent& evt) {
+  std::complex<double> val = m_model.ComplexVal(evt) ;
+  std::complex<double> cpval = m_cpmodel.ComplexVal(evt) ;
+  return std::conj(val) * cpval ;
 }
