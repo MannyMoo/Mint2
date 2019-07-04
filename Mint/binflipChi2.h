514eca0323a72dceee21437306254475566f59bf
NiallMcHugh <niall_mchugh@outlook.com>
2019-07-04 16:05:52 +0100

    Added member variables and wrote constructor, getVal and getFit member functions in binflipChi2.

--- Mint/binflipChi2.h
+++ Mint/binflipChi2.h
@@ -1,15 +1,31 @@
 #include "Mint/IMinimisable.h"
 #include "Mint/MinuitParameterSet.h"
 #include "Mint/Minimisable.h"
+#include "TH2F.h"
+#include "TGraph.h"
+#include "cmath"
+#include "complex"
 
 using namespace MINT;
 using namespace std;
 
 
 class binflipChi2 : public Minimisable{
-    
+    int m_nbinsPhase;
+    int m_nbinsTime;
+    vector<complex<float> > m_X;
+    float* m_r;
+    float* m_tAv;
+    float* m_tSqAv;
+    TH2F m_pHistD0;
+    TH2F m_pHistD0bar;
+    TH2F m_nHistD0;
+    TH2F m_nHistD0bar;
+
   public:
-    binflipChi2();
+    binflipChi2(int nbinsPhase, int nbinsTime, float* Xreal, float* Ximag, float* r, float* tAv, float* tSqAv,
+		      TH2F pHistD0, TH2F pHistD0bar, TH2F nHistD0, TH2F nHistD0bar);
     double getVal();
+    vector<vector<TGraph> > getFits(complex<float> zcp, complex<float> deltaz);
 };
 
--- src/Utils/binflipChi2.cpp
+++ src/Utils/binflipChi2.cpp
@@ -3,10 +3,91 @@
 using namespace MINT;
 using namespace std;
 
-binflipChi2::binflipChi2(){
+binflipChi2::binflipChi2(int nbinsPhase, int nbinsTime, float* Xreal, float* Ximag, float* r, float* tAv, float* tSqAv,
+                                      TH2F pHistD0, TH2F pHistD0bar, TH2F nHistD0, TH2F nHistD0bar):
+  m_nbinsPhase(nbinsPhase),
+  m_nbinsTime(nbinsTime),
+  m_r(r),
+  m_tAv(tAv),
+  m_tSqAv(tSqAv),
+  m_pHistD0(pHistD0),
+  m_pHistD0bar(pHistD0bar),
+  m_nHistD0(nHistD0),
+  m_nHistD0bar(nHistD0bar)
+{
+    for(int i = 0; i < m_nbinsPhase; i++){
+        complex<float> Xb(Xreal[i], Ximag[i]);
+        m_X.push_back(Xb);
+    }
 }
 
-double binflipChi2::getVal(){
-  double chi2 = 0. ;
-  return chi2 ;
+double binflipChi2::getVal(){    
+
+    double chi2 = 0.;
+    vector<vector<TGraph> > fits;
+    complex<float> zcp, deltaz;
+
+    fits = getFits(zcp, deltaz);
+
+    for(int b = 1; b <= m_nbinsPhase; b++){
+    
+        double* Rvals_pl = fits[0][b-1].GetY();
+        double* Rvals_mi = fits[0][b-1].GetY();  
+    
+        for(int j = 0; j <= m_nbinsTime; j++){
+	    double R_pl = Rvals_pl[j-1];
+            double R_mi = Rvals_mi[j-1]; 
+            float D0_term = 0, D0bar_term = 0;
+            float D0_num = 0, D0_den = 0, D0bar_num = 0, D0bar_den = 0; 
+
+            D0_num = pow(m_nHistD0.GetBinContent(j,b) - m_pHistD0.GetBinContent(j,b) * R_pl, 2);
+            D0_den =   pow(m_nHistD0.GetBinError(j,b), 2) + pow(m_pHistD0.GetBinError(j,b) * R_pl, 2); 
+            if(D0_den != 0){
+	        D0_term = D0_num/D0_den;
+	    }
+
+            D0bar_num = pow(m_nHistD0bar.GetBinContent(j,b) - m_pHistD0bar.GetBinContent(j,b) * R_mi, 2);
+            D0bar_den =   pow(m_nHistD0bar.GetBinError(j,b), 2) + pow(m_pHistD0bar.GetBinError(j,b) * R_mi, 2); 
+            if(D0bar_den != 0){
+	        D0bar_term = D0bar_num/D0bar_den;
+	    }
+
+            chi2 += D0_term + D0bar_term;
+            
+        }
+    }
+  
+    return chi2;
 }
+
+
+vector<vector<TGraph> > binflipChi2::getFits(complex<float >zcp, complex<float> deltaz){
+
+    vector<vector<TGraph> > fits;
+
+    for(int i : {0,1}){
+
+        for(int b = 1; b <= m_nbinsPhase; b++){
+
+            fits[i].push_back( TGraph(m_nbinsTime) );
+
+            for(int j = 1; j <= m_nbinsTime; j++){
+                float numerator = 0., denominator = 0.;
+     
+                numerator = m_r[b-1] * ( 1 + 0.25 * m_tSqAv[j-1] * ( pow(zcp,2) - pow(deltaz,2) ).real() );
+                numerator += 0.25 * m_tSqAv[j-1] * abs(zcp + (float)pow(-1, i) * pow(deltaz, 2));
+	        numerator += sqrt(m_r[b-1]) * m_tAv[j-1] * ( conj(m_X[b-1]) * (zcp + (float)pow(-1, i) * deltaz) ).real();
+
+                denominator = 1 + 0.25 * m_tSqAv[j-1] * ( pow(zcp,2) - pow(deltaz,2) ).real();
+                denominator += m_r[b-1] * 0.25 * m_tSqAv[j-1] * abs(zcp + (float)pow(-1, i) * pow(deltaz, 2));
+	        denominator += sqrt(m_r[b-1]) * m_tAv[j-1] * ( m_X[b-1] * (zcp + (float)pow(-1, i) * deltaz) ).real();
+
+                float Rval = numerator / denominator;
+	        fits[i][b-1].SetPoint(j-1, m_tAv[j-1], Rval);
+            }
+        }
+    }
+
+    return fits;
+}
+
