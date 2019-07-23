6f6e332df9d9af44c8b4f9559cfb73e6a5bbed18
NiallMcHugh <niall_mchugh@outlook.com>
2019-07-23 17:27:38 +0100

    Added class 'Eff3piSymmetric' to calculate efficiency for given s13, s23 in D0->pipipi events.

---
+++ Mint/Eff3piSymmetric.h
@@ -0,0 +1,21 @@
+#include "Mint/IReturnRealForEvent.h"
+#include "Mint/IDalitzEvent.h"
+#include "math.h"
+
+using namespace MINT;
+using namespace std;
+
+class Eff3piSymmetric : public IReturnRealForEvent<IDalitzEvent> {
+    double m_p0;
+    double m_p1;
+    double m_p2;
+    double m_p3;
+
+  public:
+    Eff3piSymmetric(double p0 = 0.599125, double p1 = 5.25471 * pow(10,-8), 
+		                double p2 = -5.83156 * pow(10,-14), double p3 = -1.80603 * pow(10,-21));
+    virtual double RealVal(IDalitzEvent& evt);
+
+};
+
+
